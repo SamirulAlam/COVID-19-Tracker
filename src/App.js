ddaf531c144ad78f50b0555d6259b6a0f3d8aa2e
Samirul Alam <samirulalam372@gmail.com>
2020-11-28 21:51:08 -0800

    circle added in the map

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Map from "./Map";
 import Table from "./Table";
 import LineGraph from "./LineGraph";
 import "leaflet/dist/leaflet.css"
+import { sortData } from "./util";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
   const [tableData,setTableData]=useState([]);
   const [mapCenter,setMapCenter]=useState({lat:34.80746, lng:-40.4796});
   const [mapZoom,setMapZoom]=useState(3);
+  const [mapCountries,setMapCountries]=useState([]);
 
   useEffect(()=>{
     fetch("https://disease.sh/v3/covid-19/all")
@@ -34,8 +36,10 @@ function App() {
             name:country.country,
             value:country.countryInfo.iso2
           }
-        ))
-        setTableData(data);
+        ));
+        const sortedData=sortData(data);
+        setTableData(sortedData);
+        setMapCountries(data);
         setCountries(countries);
         
       })
@@ -53,11 +57,12 @@ function App() {
 
       await fetch(url)
       .then(response=>response.json())
-      .then(data=>{
-        setCountry(countryCode);
-        setCountryInfo(data);
-        //setMapCenter([data.countryInfo.lat,data.countryInfo.lng]);
-        setMapZoom(5)
+      .then((data)=>{
+          setCountry(countryCode);
+          setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat,data.countryInfo.long]);
+        setMapZoom(5);
+        //console.log(mapCenter);
       })
   }
 
@@ -86,7 +91,7 @@ function App() {
                 <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
                 <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
           </div>
-          <Map center={mapCenter} zoom={mapZoom}/>
+          <Map countries={mapCountries} center={mapCenter} zoom={mapZoom}/>
       </div>
       <Card className="app__right">
         <CardContent>
--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import "./Map.css"
-import {MapContainer as LeafletMap ,  TileLayer} from "react-leaflet";
+import {MapContainer,  TileLayer} from "react-leaflet";
+import { showDataOnMap } from './util';
 
-function Map({center,zoom}) {
+function Map({countries, casesType, center,zoom}) {
     return (
         <div className="map">
-            <LeafletMap center={center} zoom={zoom}>
+            <MapContainer center={center} zoom={zoom}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            </LeafletMap>
+            {showDataOnMap(countries,casesType)}
+            </MapContainer>
         </div>
     )
 }
